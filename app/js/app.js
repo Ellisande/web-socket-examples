ecff55c443ab9a17bc298c819d41760804e14fba
unknown <justin.dragos.m91s@statefarm.com>
2014-04-03 10:05:52 -0700

    Removed extra routes from the seed app that aren't necessary. Also changed the route provider to the Angular 1.3.x style

--- app/js/app.js
+++ app/js/app.js
@@ -7,9 +7,5 @@ var app = angular.module('standup', ['standupFilters','ngRoute']).
   $routeProvider.
       //Removed temporarily while focusing on single meeting principle.
       when('/home', {templateUrl: 'partials/home.html',   controller: HomeCtrl}).
-      when('/meeting/:meetingName', {templateUrl: 'partials/meeting.html', controller: MeetingCtrl}).
-      //Removed temporarily while focusing on single meeting principle.
-      when('/snapshot', {templateUrl: "partials/snapshot.html", controller: SnapshotCtrl}).
-      // when('/snapshot', {tempalteUrl: "partials/snapshot.html", controller: CreateCtrl}).
       otherwise({redirectTo: '/home'});
 }]);
