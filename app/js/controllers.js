7bb8105f72036cdbeaf05d4e5c79f59584a5625d
Ellisande <justin.dragos@gmail.com>
2014-04-03 22:42:12 -0700

    Moved the timer to show inside the lock button. Implemented a second option for gold bars.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -19,13 +19,40 @@ function HomeCtrl($scope, $location, socket, timerService) {
         this.total = this.quantity * $scope.pricePerOunce;
     };
     
-    $scope.$watch('goldOunces.quantity', function(){
-        $scope.goldOunces.compute()
+    $scope.goldBars = {};
+    $scope.goldBars.total = 0.00;
+    $scope.goldBars.compute = function(){
+        this.total = this.quantity * 10 * $scope.pricePerOunce;
+        this.pricePerBar = $scope.pricePerOunce * 10;
+    };
+    
+    $scope.$watch('goldOunces', function(){
+        $scope.goldOunces.compute();
+        $scope.cart.compute();
+    }, true);
+    
+    $scope.$watch('goldBars', function(){
+        $scope.goldBars.compute();        
+        $scope.cart.compute();
     }, true);
     
     $scope.$watch('pricePerOunce', function(){
         $scope.goldOunces.compute();
+        $scope.goldBars.compute();
+        $scope.cart.compute();
     });
+    
+    $scope.cart = [];
+    $scope.cart.push($scope.goldBars);
+    $scope.cart.push($scope.goldOunces);
+    $scope.cart.total = 0.00;
+    $scope.cart.compute = function(){
+        var sum = 0;
+        for(var i = 0; i < $scope.cart.length; i++){
+            sum += $scope.cart[i].total || 0.0;
+        }
+        this.total = sum;
+    }
     
     socket.on('updatePrice', function(data){
         if($scope.timer.expired){
