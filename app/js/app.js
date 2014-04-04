46667cc37e0289834aeebfa3f88aaaf0ca4a8f1a
unknown <justin.dragos.m91s@statefarm.com>
2014-04-04 16:08:02 -0700

    Added a new route for the analytics page.

--- app/js/app.js
+++ app/js/app.js
@@ -2,10 +2,11 @@
 
 /* App Module */
 
-var app = angular.module('standup', ['standupFilters','ngRoute']).
+var app = angular.module('standup', ['standupFilters','ngRoute','directives','services']).
   config(['$routeProvider', function($routeProvider) {
   $routeProvider.
       //Removed temporarily while focusing on single meeting principle.
       when('/home', {templateUrl: 'partials/home.html',   controller: HomeCtrl}).
+      when('/analytics', {templateUrl: 'partials/analytics.html', controller: AnalyticsCtrl}).
       otherwise({redirectTo: '/home'});
 }]);
