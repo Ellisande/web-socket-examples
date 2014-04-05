25e9a645e5aa69308bdfb38a21308965242978bc
mdskd <mdavis0816@gmail.com>
2014-04-04 18:31:03 -0700

    Changes to server and package json. Service change related.

--- server.js
+++ server.js
@@ -1,35 +1,50 @@
-
 /**
  * Module dependencies.
  */
 
 var express = require('express'),
-  routes = require('./server');
+    routes = require('./server'),
+    socket = require('./server/socket.js');
 
 var app = module.exports = express.createServer();
 
 // Hook Socket.io into Express
-var io = require('socket.io').listen(app, {log: false});
-//var io = require('socket.io').listen(app);
+var io = require('socket.io').listen(app);
 
 // Configuration
 
-app.configure(function(){
-  app.use(express.static(__dirname + '/app'));
+app.configure(function () {
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(express.static(__dirname + '/app'));
+    app.use(app.router);
 });
 
-app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+app.configure('development', function () {
+    app.use(express.errorHandler({
+        dumpExceptions: true,
+        showStack: true
+    }));
 });
 
-app.configure('production', function(){
-  app.use(express.errorHandler());
+app.configure('production', function () {
+    app.use(express.errorHandler());
 });
 
-var socket = require('./server/socket');
-io.on('connection', socket);
+// Routes
 
-app.settings.env = 'production';
-app.listen(3000, function(){
-  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+app.get('/', routes.index);
+app.get('/partials/:name', routes.partials);
+
+// redirect all others to the index (HTML5 history)
+app.get('*', routes.index);
+
+// Socket.io Communication
+
+io.sockets.on('connection', socket);
+
+// Start server
+
+app.listen(3000, function () {
+    console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
